77e65c5c671d8b4ce8a7f5bf524b7a82aed2aa3d
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-02-23 19:09:14 -0600

    context

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Signup from './pages/Signup';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LogIn from './pages/LogIn';
 import Search from './pages/Search';
 import NavBar from './components/NavBar';
-import ConsumerContext from './utils/Context/UserContext';
+import UserContext from './utils/Context/UserContext';
+import AuthDataProvider from './utils/Context/AuthDataProvider';
 
 function App() {
+  const [user, setUser] = useState({
+    loggedIn: false,
+    email: '',
+    id: '',
+    cart: []
+  });
   return (
     <Router>
-      <div className="App">
-        <NavBar></NavBar>
-        <Switch>
-          <Route exact path={['/', '/signup']} component={Signup} />
-          <Route exact path={'/login'} component={LogIn} />
-          <Route path={'/search'} component={Search} />
-        </Switch>
-      </div>
+      <AuthDataProvider>
+        <div className="App">
+          {/* do post of is auth
+         ?run is Authcompenents: components */}
+          <NavBar></NavBar>
+          <Switch>
+            <Route exact path={['/', '/signup']} component={Signup} />
+            <Route exact path={'/login'} component={LogIn} />
+            <Route path={'/search'} component={Search} />
+          </Switch>
+        </div>
+      </AuthDataProvider>
     </Router>
   );
 }
