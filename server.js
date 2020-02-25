626fe0ea110154aad794aae22871190d6daf8023
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-02-25 15:28:31 -0600

    revert

--- server.js
+++ server.js
@@ -31,19 +31,14 @@ app.use(function(err, req, res, next) {
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reactcms');
-app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+  });
+}
 
 // Start the API server
 app.listen(PORT, function() {
